32729aa7c9de7e2516908ac4ab82e3bc9c8de4a7
辣手小火星 <fanshij@163.com>
2014-08-14 16:19:59 +0800

    product
    product

---
+++ FSJPassword/FSJPassword/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FSJPassword
+//
+//  Created by yto on 14-8-14.
+//  Copyright (c) 2014年 yto_yewankun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
